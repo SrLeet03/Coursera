8c2784688708f1eb868aa5de6de22da91699c9fb
ant-hunt123 <sarveshraut123@gmail.com>
2020-05-13 14:21:17 +0530

    component part 2

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Menu from './components/MenuComponent';
+import { DISHES } from './shared/dishes';
 
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
  render ()
  {
-      return(
-               <Menu />
+  
+  return (
+    <Menu dishes={this.state.dishes} />
+
       ) ;
 
  }  
