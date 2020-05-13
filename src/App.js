7b0066f0d54026207b8f15f9c5182b94c73183e8
ant-hunt123 <sarveshraut123@gmail.com>
2020-05-13 14:56:44 +0530

    presentational and container components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,16 @@
-import React from 'react';
+import React , {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Menu from './components/MenuComponent';
-import { DISHES } from './shared/dishes';
+import Main from './components/MainComponent';
 
+class App extends Component {
 
-
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      dishes: DISHES
-    };
+  render() {
+    return (
+      <div className="App">
+        <Main />
+      </div>
+    );
   }
-
- render ()
- {
-  
-  return (
-    <Menu dishes={this.state.dishes} />
-
-      ) ;
-
- }  
 }
 export default App;
